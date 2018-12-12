e0fe32af2feeca1cf4efa16896292c34702d00d2
Ajesh <ajesh@prixa.org>
2018-12-12 11:00:09 +0545

    v0.0.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.argv[-1] == 'publish':
 
 setuptools.setup(
 	name="nepali",
-	version="0.0.3",
+	version="0.0.4",
 	author="Ajesh Sen Thapa",
 	author_email="aj3sshh@gmail.com",
 	description="Python package for nepali processes ",
