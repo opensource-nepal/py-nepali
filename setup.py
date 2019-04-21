e9fc3eeba20268e44f137c567bfd1acebf9c0677
Ajesh <ajesh@prixa.org>
2019-04-21 19:43:41 +0545

    setup removes previous distrubution versions

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ if sys.argv[-1] == 'publish':
 	if os.system("pip freeze | grep twine"):
 		print("twine not installed.\nUse `pip install twine`.\nExiting.")
 		sys.exit()
+	os.system("rm -rf dist")
 	os.system("python3 setup.py sdist bdist_wheel")
 	os.system("twine upload dist/*")
 	sys.exit()
@@ -17,7 +18,7 @@ if sys.argv[-1] == 'publish':
 
 setuptools.setup(
 	name="nepali",
-	version="0.1.1",
+	version="0.1.2",
 	author="Ajesh Sen Thapa",
 	author_email="aj3sshh@gmail.com",
 	description="Python package for nepali processes",
