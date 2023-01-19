1fa1c3b735bff4db24ee3f0a6f2189f7385a425d
Ajesh Sen Thapa <aj3sshh@gmail.com>
2023-01-19 20:53:16 +0545

    More functions added for phone number with test cases

--- nepali/phone_number.py
+++ nepali/phone_number.py
@@ -17,36 +17,69 @@ class Operators(Enum):
         return self.value
 
 
+def is_mobile_number(number: str) -> bool:
+    """
+    Returns True is the input number is mobile number.
+
+    >>> is_mobile = is_mobile_number(number)
+    >>> if is_mobile:
+    >>>     ...
+    """
+    try:
+        return bool(_mobile_number_re.match(number))
+    except:
+        return False
+
+
+def is_landline_number(number: str) -> bool:
+    """
+    Returns True is the input number is mobile number.
+
+    >>> is_mobile = is_mobile_number(number)
+    >>> if is_mobile:
+    >>>     ...
+    """
+    try:
+        return bool(_landline_number_re.match(number))
+    except:
+        return False
+
+
+def is_valid(number: str) -> bool:
+    return is_mobile_number(number) or is_landline_number(number)
+
+
+def get_exact_number(number: str) -> str:
+    # replacing start 977
+    if number.startswith('977'):
+        number = number.replace('977', '')
+    # replacing +977 and all -
+    return number.replace('+977', '').replace('-', '')
+
+
 def parse(number: str):
+    """
+    TODO: To be implemented
+    """
     if not number and type(number) != str:
         return None
 
     number = number.replace('-', '')
 
     # checking if mobile number
-    match = _mobile_number_re.match(number)
-    if match:
+    if is_mobile_number(number):
         return _parse_mobile_number(number)
 
-    match = _landline_number_re.match(number)
-    if match:
+    if is_landline_number(number):
         return _parse_mobile_number(number)
 
     return None
 
-def get_exact_number(number: str) -> str:
-    # replacing start 977
-    if number.startswith('977'):
-        number.replace('977', '')
-
-    # replacing +977
-    number.replace('+977', '')
-
-    # replacing all -
-    number.replace('-', '')
-    return number
 
 def _parse_mobile_number(number: str) -> dict:
+    """
+    TODO: To be implemented
+    """
     number = get_exact_number(number)
     detail = {
         "type": "Mobile",
@@ -54,7 +87,11 @@ def _parse_mobile_number(number: str) -> dict:
     }
     return detail
 
+
 def _parse_landline_number(number) -> dict:
+    """
+    TODO: To be implemented
+    """
     number = get_exact_number(number)
     return {
         "type": "Landline",
--- nepali/tests/test_phone_number.py
+++ nepali/tests/test_phone_number.py
@@ -1,54 +1,94 @@
+"""
+NOTE: To run only this test case use the below command
+python setup.py test -s nepali.tests.test_phone_number
+"""
 import unittest
-from nepali import phone_number
+from unittest import mock
+
+from nepali.phone_number import is_mobile_number, is_landline_number, is_valid, get_exact_number, parse
 
 
 class TestPhoneNumber(unittest.TestCase):
-    def test_parse_invalid_argument(self):
-        # string input
-        number = phone_number.parse('test')
-        self.assertEqual(number, None)
+    def test_is_mobile_number(self):
+        # True case
+        self.assertEqual(is_mobile_number('9842531781'), True)
+        self.assertEqual(is_mobile_number('9779842531781'), True)
+        self.assertEqual(is_mobile_number('+9779842531781'), True)
+        self.assertEqual(is_mobile_number('+977-9842531781'), True)
 
-        # None input
-        number = phone_number.parse(None)
-        self.assertEqual(number, None)
+        # False case
+        self.assertEqual(is_mobile_number(None), False)  # type: ignore
+        self.assertEqual(is_mobile_number(''), False)
+        self.assertEqual(is_mobile_number('test'), False)
+        self.assertEqual(is_mobile_number('977test'), False)
+        self.assertEqual(is_mobile_number('+977test'), False)
+        self.assertEqual(is_mobile_number('+977-test'), False)
 
-        # invalid number
-        number = phone_number.parse('987678')
-        self.assertEqual(number, None)
+    def test_is_landline_number(self):
+        # True case
+        self.assertEqual(is_landline_number('14231481'), True)
+        self.assertEqual(is_landline_number('014231481'), True)
+        self.assertEqual(is_landline_number('0215154579'), True)
+        self.assertEqual(is_landline_number('+977-142314819'), True)
+        self.assertEqual(is_landline_number('0154225639'), True)
+        self.assertEqual(is_landline_number('9770154225639'), True)
+        self.assertEqual(is_landline_number('0565114679'), True)
+        self.assertEqual(is_landline_number('+977-0565114679'), True)
 
-    def test_valid_mobile_number_input(self):
-        number = phone_number.parse('9842531781')
-        self.assertEqual(type(number), dict)
+        # False case
+        self.assertEqual(is_landline_number(None), False)  # type: ignore
+        self.assertEqual(is_landline_number(''), False)
+        self.assertEqual(is_landline_number('test'), False)
+        self.assertEqual(is_landline_number('977test'), False)
+        self.assertEqual(is_landline_number('+977test'), False)
+        self.assertEqual(is_landline_number('+977-test'), False)
 
-        number = phone_number.parse('+9779842531781')
-        self.assertEqual(type(number), dict)
+    def test_is_valid(self):
+        # mock start
+        patcher1 = mock.patch('nepali.phone_number.is_mobile_number')
+        patcher2 = mock.patch('nepali.phone_number.is_landline_number')
+        mock_is_mobile_number = patcher1.start()
+        mock_is_landline_number = patcher2.start()
 
-        number = phone_number.parse('+977-9842531781')
-        self.assertEqual(type(number), dict)
-        self.assertEqual(number and number['type'], 'Mobile')
+        # both mobile and landline are invalid
+        mock_is_mobile_number.return_value = False
+        mock_is_landline_number.return_value = False
+        self.assertEqual(is_valid('who_cares'), False)
 
-    def test_valid_phone_number_input(self):
-        number = phone_number.parse('14231481')
-        self.assertEqual(type(number), dict)
+        # mobile is invalid and landline is valid
+        mock_is_mobile_number.return_value = True
+        mock_is_landline_number.return_value = False
+        self.assertEqual(is_valid('who_cares'), True)
 
-        number = phone_number.parse('014231481')
-        self.assertEqual(type(number), dict)
+        # mobile is invalid and landline is valid
+        mock_is_mobile_number.return_value = False
+        mock_is_landline_number.return_value = True
+        self.assertEqual(is_valid('who_cares'), True)
 
-        number = phone_number.parse('0215154579')
-        self.assertEqual(type(number), dict)
+        # both mobile and landline are valid
+        mock_is_mobile_number.return_value = True
+        mock_is_landline_number.return_value = True
+        self.assertEqual(is_valid('who_cares'), True)
 
-        number = phone_number.parse('+977-142314819')
-        self.assertEqual(type(number), dict)
+        # mock stop
+        patcher1.stop()
+        patcher2.stop()
 
-        number = phone_number.parse('0154225639')
-        self.assertEqual(type(number), dict)
-
-        number = phone_number.parse('9770154225639')
-        self.assertEqual(type(number), dict)
-
-        number = phone_number.parse('0565114679')
-        self.assertEqual(type(number), dict)
-
-        number = phone_number.parse('+977-0565114679')
-        self.assertEqual(type(number), dict)
-        self.assertEqual(number and number['type'], 'Mobile')
+    def test_get_exact_number(self):
+        self.assertEqual(get_exact_number('9842531781'), '9842531781')
+        self.assertEqual(get_exact_number('9779842531781'), '9842531781')
+        self.assertEqual(get_exact_number('+9779842531781'), '9842531781')
+        self.assertEqual(get_exact_number('+977-9842531781'), '9842531781')
+        with self.assertRaises(AttributeError):
+            get_exact_number(None)  # type: ignore
+        self.assertEqual(get_exact_number(''), '')
+        self.assertEqual(get_exact_number('test'), 'test')
+        self.assertEqual(get_exact_number('977test'), 'test')
+        self.assertEqual(get_exact_number('+977test'), 'test')
+        self.assertEqual(get_exact_number('+977-test'), 'test')
+        self.assertEqual(get_exact_number('+977-142314819'), '142314819')
+        self.assertEqual(get_exact_number('0154225639'), '0154225639')
+        self.assertEqual(get_exact_number('9770154225639'), '0154225639')
+        self.assertEqual(get_exact_number('0565114679'), '0565114679')
+        self.assertEqual(get_exact_number('984-2531781'), '9842531781')
+        self.assertEqual(get_exact_number('984-2531-781'), '9842531781')
