0c7ea4e4a076d34906a4ecf68257d92243f5112a
Ajesh Sen Thapa <aj3sshh@gmail.com>
2023-01-25 22:50:57 +0545

    Added typing on timezone and utils

--- nepali/timezone.py
+++ nepali/timezone.py
@@ -1,13 +1,19 @@
 import datetime
+from typing import Union
+
 
 class NepaliTimeZone(datetime.tzinfo):
+    """
+    NepaliTimeZone: "Asia/Kathmandu", +05:45
+    """
+
     def utcoffset(self, dt):
         return self.dst(dt) + datetime.timedelta(hours=5, minutes=45)
 
     def dst(self, dt):
         return datetime.timedelta(0)
-    
-    def tzname(self,dt):
+
+    def tzname(self, dt):
         return "Asia/Kathmandu"
 
     def __str__(self):
@@ -16,12 +22,20 @@ class NepaliTimeZone(datetime.tzinfo):
     def __repr__(self):
         return "Asia/Kathmandu"
 
-def get_timezone():
+
+def get_timezone() -> Union[datetime.tzinfo, None]:
+    """
+    Returns current device's timezone.
+    Timezone of the machine.
+    """
     return datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
 
-def now():
+
+def now() -> datetime.datetime:
+    """Returns current datetime object of the device"""
     return datetime.datetime.now(get_timezone())
 
-def utc_now():
-    return datetime.datetime.now(datetime.timezone.utc)
 
+def utc_now() -> datetime.datetime:
+    """Returns UTC time datetime object"""
+    return datetime.datetime.now(datetime.timezone.utc)
--- nepali/utils.py
+++ nepali/utils.py
@@ -1,50 +1,65 @@
 import datetime
 import pytz
-import warnings
+from typing import Any
 
 from .timezone import NepaliTimeZone, get_timezone
 from .exceptions import InvalidNepaliDateTimeObjectException
 
-def to_utc_timezone(datetime_obj):
-	if type(datetime_obj) != datetime.datetime:
-		# None datetime object
-		return datetime_obj
 
-	if (not hasattr(datetime_obj, 'tzinfo')) or (not datetime_obj.tzinfo):
-		datetime_obj = datetime_obj.replace(tzinfo=get_timezone())
-	return datetime_obj.astimezone(pytz.timezone('UTC'))
+# TODO: Move timezone methods to timezone.py
 
+def to_utc_timezone(datetime_obj: datetime.datetime) -> datetime.datetime:
+    """Changes the timezone of the given datetime object to UTC."""
+    if type(datetime_obj) != datetime.datetime:
+        # None datetime object
+        return datetime_obj
 
-def to_nepali_timezone(datetime_obj):
-	if type(datetime_obj) != datetime.datetime:
-		return datetime_obj
-
-	if (not hasattr(datetime_obj, 'tzinfo')) or (not datetime_obj.tzinfo):
-		datetime_obj = datetime_obj.replace(tzinfo=get_timezone())
-	return datetime_obj.astimezone(NepaliTimeZone())
+    if (not hasattr(datetime_obj, "tzinfo")) or (not datetime_obj.tzinfo):
+        datetime_obj = datetime_obj.replace(tzinfo=get_timezone())
+    return datetime_obj.astimezone(pytz.timezone("UTC"))
 
 
-def to_nepalidatetime(datetime_object):
-	"""
-	Converts nepalidate, datetime.datetime, datetime.date to nepalidatetime. 
-	"""
-	from .datetime import nepalidate, nepalidatetime
+def to_nepali_timezone(datetime_obj: datetime.datetime) -> datetime.datetime:
+    """Changes the timezone of the given datetime object to NepaliTimeZone."""
+    if type(datetime_obj) != datetime.datetime:
+        return datetime_obj
 
-	if type(datetime_object) == nepalidatetime:
-		return datetime_object
-	elif type(datetime_object) == nepalidate:
-		return nepalidatetime.from_nepali_date(datetime_object)
-	elif type(datetime_object) == datetime.datetime:
-		return nepalidatetime.from_datetime(datetime_object)
-	elif type(datetime_object) == datetime.date:
-		return nepalidatetime.from_date(datetime_object)
-	elif datetime_object == '' or datetime_object == None:
-		return None
-	raise InvalidNepaliDateTimeObjectException('Argument must be instance of nepalidate or nepalidatetime or datetime.datetime or datetime.date') 
+    if (not hasattr(datetime_obj, "tzinfo")) or (not datetime_obj.tzinfo):
+        datetime_obj = datetime_obj.replace(tzinfo=get_timezone())
+    return datetime_obj.astimezone(NepaliTimeZone())
 
 
-def to_nepalidate(datetime_object):
-	nepalidatetime_obj = to_nepalidatetime(datetime_object)
-	if nepalidatetime_obj != None:
-		return nepalidatetime_obj.date()
-	return 
+# TODO: Move inside the datetime
+
+def to_nepalidatetime(datetime_object: Any) -> Any:
+    """
+    Converts nepalidate, datetime.datetime, datetime.date to nepalidatetime.
+
+    :return: nepalidatetime
+    """
+    from .datetime import nepalidate, nepalidatetime
+
+    if type(datetime_object) == nepalidatetime:
+        return datetime_object
+    elif type(datetime_object) == nepalidate:
+        return nepalidatetime.from_nepali_date(datetime_object)
+    elif type(datetime_object) == datetime.datetime:
+        return nepalidatetime.from_datetime(datetime_object)
+    elif type(datetime_object) == datetime.date:
+        return nepalidatetime.from_date(datetime_object)
+    elif datetime_object == "" or datetime_object == None:
+        return None
+    raise InvalidNepaliDateTimeObjectException(
+        "Argument must be instance of nepalidate or nepalidatetime or datetime.datetime or datetime.date")
+
+
+def to_nepalidate(datetime_object: Any) -> Any:
+    """
+    Converts nepalidate, datetime.datetime, datetime.date to nepalidate.
+
+    :return: nepalidate
+    """
+    nepalidatetime_obj = to_nepalidatetime(datetime_object)
+    if nepalidatetime_obj != None:
+        return nepalidatetime_obj.date()
+    return None
