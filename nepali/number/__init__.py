bbff3b35791f3cecddae8c8641bbd125ac9ed028
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2023-02-24 20:33:17 +0545

    Feature: Add skeleton for class nepalinumber

---
+++ nepali/number/__init__.py
@@ -0,0 +1,17 @@
+from ._number import NepaliNumber
+from .utils import (
+    add_comma,
+    add_comma_english,
+    convert_and_add_comma,
+    english_to_nepali,
+    nepali_to_english,
+)
+
+__all__ = [
+    "NepaliNumber",
+    "add_comma",
+    "add_comma_english",
+    "convert_and_add_comma",
+    "english_to_nepali",
+    "nepali_to_english",
+]
---
+++ nepali/number/nepalinumber.py
@@ -0,0 +1,17 @@
+"""
+Contains the class for the nepalinumber feature
+"""
+
+
+class nepalinumber:
+    """
+    Represents the nepali(devanagari) numbers and
+    the features related to arithmetic operations
+    on them
+    """
+
+    def __init__(self, value) -> None:
+        """
+        Constructor/Initializer
+        """
+        self.value = value
