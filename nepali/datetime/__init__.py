f196fe88887832274f98e558bc94259e32ddaf18
Ajesh Sen Thapa <aj3sshh@gmail.com>
2023-03-08 19:55:54 +0545

    nepalimonth initialization

--- nepali/datetime/__init__.py
+++ nepali/datetime/__init__.py
@@ -1,12 +1,14 @@
 from ._datetime import nepalidate, nepalitime, nepalidatetime
 from ._formatter import NepaliDateTimeFormatter
 from ._humanize import HumanizeDateTime, nepalihumanize
+from ._nepalimonth import nepalimonth
 
 __all__ = [
     "nepalidate",
     "nepalitime",
     "nepalidatetime",
     "nepalihumanize",
+    "nepalimonth",
     "NepaliDateTimeFormatter",
     "HumanizeDateTime",
 ]
---
+++ nepali/datetime/_nepalimonth.py
@@ -0,0 +1,87 @@
+from typing import Union
+
+MONTHS_EN = [
+    "Baishakh",
+    "Jestha",
+    "Ashad",
+    "Sharwan",
+    "Bhadra",
+    "Ashwin",
+    "Kartik",
+    "Mangsir",
+    "Poush",
+    "Magh",
+    "Falgun",
+    "Chaitra",
+]
+
+MONTHS_NE = [
+    "बैशाख",
+    "जेठ",
+    "असार",
+    "साउन",
+    "भदौ",
+    "असोज",
+    "कात्तिक",
+    "मंसिर",
+    "पुस",
+    "माघ",
+    "फागुन",
+    "चैत",
+]
+
+
+class nepalimonth_meta(type):
+    def __call__(cls, month: Union[int, str], *args, **kwargs):
+        """
+        Parses the month data and manages the cache.
+        """
+        value = None
+
+        if isinstance(month, int):
+            value = int(month)
+        elif isinstance(month, str):
+            value = cls._parse_str(month)
+
+        if value is None or value < 1 or value > 12:
+            raise ValueError(f"Invalid month: {month}")
+
+        return super(nepalimonth_meta, cls).__call__(value, *args, **kwargs)
+
+    def _parse_str(cls, month: str) -> int:
+        """
+        Parses str value of the month and returns int
+        """
+        if month.isdigit():
+            return int(month)
+
+        try:
+            return (MONTHS_EN + MONTHS_NE).index(month.capitalize()) % 12 + 1
+        except ValueError:
+            return -1  # invalid month range
+
+
+class nepalimonth(metaclass=nepalimonth_meta):
+    """
+    Represents Nepali month: Baishakh, Jestha, ..., Chaitra.
+    Baishak: 1,
+    Jestha: 2,
+    ...
+    Chaitra: 3
+
+    >>> nepalimonth(1)
+    >>> nepalimonth("Baishakh")
+    >>> nepalimonth("बैशाख")
+
+    :param Union[int, str] month: Month data to be parsed.
+
+    :raises TypeError: Invalid object type is passed.
+    :raises ValueError: The value is invalid.
+    """
+
+    def __init__(self, month) -> None:
+        self._value = month
+
+    @property
+    def value(self):
+        return self._value
