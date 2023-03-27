334b37d489985f128456bb35d5f08957334e518e
Ajesh Sen Thapa <aj3sshh@gmail.com>
2023-03-27 21:17:08 +0545

    Optimization and refactoring for nepalimonth and nepaliweek

--- nepali/datetime/_nepalimonth.py
+++ nepali/datetime/_nepalimonth.py
@@ -1,21 +1,32 @@
-from typing import Any, Dict, Union
+from functools import cached_property
+from typing import Any, Dict, Optional, Union
 
 from .constants import MONTHS_EN, MONTHS_NE
 
 
-class nepalimonth_meta(type):
+class NepaliMonthMeta(type):
     _cache: Dict[int, "nepalimonth"] = {}
 
-    def __call__(cls, month: Union[int, str], *args, **kwargs):
+    def __call__(cls, month: Union[int, str], *args, **kwargs) -> "nepalimonth":
         """
         Parses the month data and manages the cache.
+
+        :param month: An integer or string representing the month.
+        :type month: Union[int, str]
+        :return: An instance of the nepalimonth class representing the given month.
+        :rtype: nepalimonth
+        :raises ValueError: If the given month is invalid.
         """
+        value: Optional[int] = None
         value = None
 
         if isinstance(month, int):
             value = int(month)
         elif isinstance(month, str):
-            value = cls._parse_str(month)
+            try:
+                value = cls._parse_str(month)
+            except ValueError:
+                pass
 
         # checking if month is valid
         if value is None or value < 1 or value > 12:
@@ -23,7 +34,7 @@ class nepalimonth_meta(type):
 
         # checking cache
         if value not in cls._cache:
-            cls._cache[value] = super(nepalimonth_meta, cls).__call__(
+            cls._cache[value] = super(NepaliMonthMeta, cls).__call__(
                 value, *args, **kwargs
             )
 
@@ -31,18 +42,28 @@ class nepalimonth_meta(type):
 
     def _parse_str(cls, month: str) -> int:
         """
-        Parses str value of the month and returns int
+        Parses str value of the month and returns int.
+
+        :param month: A string representing the month.
+        :type month: str
+        :return: An integer representing the month.
+        :rtype: int
+        :raises ValueError: If the given string does not represent a valid month.
         """
         if month.isdigit():
             return int(month)
 
+        month = month.capitalize()
+        month_names = MONTHS_EN + MONTHS_NE
         try:
-            return (MONTHS_EN + MONTHS_NE).index(month.capitalize()) % 12 + 1
+            index = month_names.index(month)
         except ValueError:
-            return -1  # invalid month range
+            raise ValueError(f"Invalid month name: {month}")
+
+        return (index % 12) + 1
 
 
-class nepalimonth(metaclass=nepalimonth_meta):
+class nepalimonth(metaclass=NepaliMonthMeta):
     """
     Represents Nepali month: Baishakh, Jestha, ..., Chaitra.
     Baishak: 1,
@@ -60,33 +81,35 @@ class nepalimonth(metaclass=nepalimonth_meta):
     """
 
     def __init__(self, month) -> None:
-        self._value = month
+        self.__value = month
 
     def __str__(self) -> str:
         return self.name
 
     def __repr__(self) -> str:
-        return f"<nepalimonth: {self._value}>"
+        return f"<nepalimonth: {self.__value}>"
 
     def __int__(self) -> int:
         return self.value
 
     def __eq__(self, other: Any) -> bool:
-        try:
-            return self.value == int(other)
-        except:
-            return False
+        if isinstance(other, nepalimonth):
+            return self.value == other.value
+        elif isinstance(other, int):
+            return self.value == other
 
-    @property
+        return False
+
+    @cached_property
     def value(self) -> int:
-        return self._value
+        return self.__value
 
-    @property
+    @cached_property
     def name(self) -> str:
         """Month's english name"""
-        return MONTHS_EN[self._value - 1]
+        return MONTHS_EN[self.__value - 1]
 
-    @property
+    @cached_property
     def name_ne(self) -> str:
         """Month's nepali name"""
-        return MONTHS_NE[self._value - 1]
+        return MONTHS_NE[self.__value - 1]
--- nepali/datetime/_nepaliweek.py
+++ nepali/datetime/_nepaliweek.py
@@ -1,50 +1,66 @@
-from typing import Any, Dict, Union
+from functools import cached_property
+from typing import Any, Dict, Optional, Union
 
 from .constants import WEEKS_EN, WEEKS_NE, WEEKS_ABBR_EN, WEEKS_ABBR_NE
 
 
-class nepaliweek_meta(type):
+class NepaliWeekMeta(type):
     _cache: Dict[int, "nepaliweek"] = {}
 
-    def __call__(cls, week: Union[int, str], *args, **kwargs):
+    def __call__(cls, week: Union[int, str], *args, **kwargs) -> "nepaliweek":
         """
         Parses the week data and manages the cache.
+
+        :param week: An integer or string representing the week.
+        :type week: Union[int, str]
+        :return: An instance of the nepaliweek class representing the given week.
+        :rtype: nepaliweek
+        :raises ValueError: If the given week is invalid.
         """
-        value = None
+        value: Optional[int] = None
 
         if isinstance(week, int):
             value = int(week)
         elif isinstance(week, str):
-            value = cls._parse_str(week)
+            try:
+                value = cls._parse_str(week)
+            except ValueError:
+                pass
 
         # checking if week is valid
-        if value is None or value < 1 or value > 7:
+        if value is None or not (1 <= value <= 7):
             raise ValueError(f"Invalid week: {week}")
 
         # checking cache
         if value not in cls._cache:
-            cls._cache[value] = super(nepaliweek_meta, cls).__call__(
-                value, *args, **kwargs
-            )
+            cls._cache[value] = super().__call__(value, *args, **kwargs)
 
         return cls._cache[value]
 
     def _parse_str(cls, week: str) -> int:
         """
-        Parses str value of the week and returns int
+        Parses str value of the week and returns int.
+
+        :param week: A string representing the week.
+        :type week: str
+        :return: An integer representing the week.
+        :rtype: int
+        :raises ValueError: If the given string does not represent a valid week.
         """
         if week.isdigit():
             return int(week)
 
+        week = week.capitalize()
+        week_names = WEEKS_EN + WEEKS_NE + WEEKS_ABBR_EN + WEEKS_ABBR_NE
         try:
-            return (WEEKS_EN + WEEKS_NE + WEEKS_ABBR_EN + WEEKS_ABBR_NE).index(
-                week.capitalize()
-            ) % 7 + 1
+            index = week_names.index(week)
         except ValueError:
-            return -1  # invalid week range
+            raise ValueError(f"Invalid week name: {week}")
+
+        return (index % 7) + 1
 
 
-class nepaliweek(metaclass=nepaliweek_meta):
+class nepaliweek(metaclass=NepaliWeekMeta):
     """
     Represents Nepali week: Sunday, Monday, ..., Saturday.
     Sunday: 1,
@@ -63,43 +79,45 @@ class nepaliweek(metaclass=nepaliweek_meta):
     """
 
     def __init__(self, week) -> None:
-        self._value = week
+        self.__value = week
 
     def __str__(self) -> str:
         return self.name
 
     def __repr__(self) -> str:
-        return f"<nepaliweek: {self._value}>"
+        return f"<nepaliweek: {self.__value}>"
 
     def __int__(self) -> int:
         return self.value
 
     def __eq__(self, other: Any) -> bool:
-        try:
-            return self.value == int(other)
-        except:
-            return False
+        if isinstance(other, nepaliweek):
+            return self.value == other.value
+        elif isinstance(other, int):
+            return self.value == other
 
-    @property
+        return False
+
+    @cached_property
     def value(self) -> int:
-        return self._value
+        return self.__value
 
-    @property
+    @cached_property
     def name(self) -> str:
         """Week's english name"""
-        return WEEKS_EN[self._value - 1]
+        return WEEKS_EN[self.__value - 1]
 
-    @property
+    @cached_property
     def abbr(self) -> str:
         """Week's english abbreviated name"""
-        return WEEKS_ABBR_EN[self._value - 1]
+        return WEEKS_ABBR_EN[self.__value - 1]
 
-    @property
+    @cached_property
     def name_ne(self) -> str:
         """Week's nepali name"""
-        return WEEKS_NE[self._value - 1]
+        return WEEKS_NE[self.__value - 1]
 
-    @property
+    @cached_property
     def abbr_ne(self) -> str:
         """Week's nepali abbreviated name"""
-        return WEEKS_ABBR_NE[self._value - 1]
+        return WEEKS_ABBR_NE[self.__value - 1]
