8690c14ea632883249ee2d3618f8a24e52299b42
Tailung <tailung12345678910@gmail.com>
2026-03-04 13:55:48 +0545

    feat: add area name to telephone number
    reference: https://www.prokerala.com/how-to/call/nepal/area-codes/?utm_source=chatgpt.com
    potential issue: some districts use same code, eg: gandaki and gorkha
    , might not return the expected district

--- nepali/phone_number.py
+++ nepali/phone_number.py
@@ -21,6 +21,113 @@ class Operator(Enum):
         return f"<Operator: {self.value}>"
 
 
+class Area(Enum):
+    # Koshi Province
+    BHOJPUR = ("Bhojpur", "029")
+    DHANKUTA = ("Dhankuta", "026")
+    ILAM = ("Ilam", "027")
+    JHAPA = ("Jhapa", "023")
+    KHOTANG = ("Khotang", "036")
+    MORANG = ("Morang", "021")
+    OKHALDHUNGA = ("Okhaldhunga", "037")
+    PANCHTHAR = ("Panchthar", "024")
+    SANKHUWASABHA = ("Sankhuwasabha", "029")
+    SOLUKHUMBU = ("Solukhumbu", "038")
+    SUNSARI = ("Sunsari", "025")
+    TAPLEJUNG = ("Taplejung", "024")
+    TERHATHUM = ("Terhathum", "026")
+    UDAYAPUR = ("Udayapur", "035")
+
+    # Madhesh Province
+    BARA = ("Bara", "053")
+    DHANUSA = ("Dhanusa", "041")
+    MAHOTTARI = ("Mahottari", "044")
+    PARSA = ("Parsa", "051")
+    RAUTAHAT = ("Rautahat", "055")
+    SAPTARI = ("Saptari", "031")
+    SARLAHI = ("Sarlahi", "046")
+    SIRAHA = ("Siraha", "033")
+
+    # Bagmati Province
+    BHAKTAPUR = ("Bhaktapur", "01")
+    CHITWAN = ("Chitwan", "056")
+    DHADING = ("Dhading", "010")
+    DOLAKHA = ("Dolakha", "049")
+    KATHMANDU = ("Kathmandu", "01")
+    KAVREPALANCHOK = ("Kavrepalanchok", "011")
+    LALITPUR = ("Lalitpur", "01")
+    MAKWANPUR = ("Makwanpur", "057")
+    NUWAKOT = ("Nuwakot", "010")
+    RAMECHHAP = ("Ramechhap", "048")
+    RASUWA = ("Rasuwa", "010")
+    SINDHUPALCHOK = ("Sindhupalchok", "011")
+    SINDHULI = ("Sindhuli", "047")
+
+    # Gandaki Province
+    BAGLUNG = ("Baglung", "068")
+    GORKHA = ("Gorkha", "064")
+    KASKI = ("Kaski", "061")
+    LAMJUNG = ("Lamjung", "066")
+    MANANG = ("Manang", "066")
+    MUSTANG = ("Mustang", "069")
+    MYAGDI = ("Myagdi", "069")
+    NAWALPUR = ("Nawalpur", "078")
+    PARBAT = ("Parbat", "067")
+    SYANGJA = ("Syangja", "063")
+    TANAHU = ("Tanahu", "065")
+
+    # Lumbini Provinc
+    ARGHAKHANCHI = ("Arghakhanchi", "077")
+    BANKE = ("Banke", "081")
+    BARDIYA = ("Bardiya", "084")
+    DANG = ("Dang", "082")
+    GULMI = ("Gulmi", "079")
+    KAPILVASTU = ("Kapilvastu", "076")
+    PARASI = ("Parasi", "078")
+    PALPA = ("Palpa", "075")
+    PYUTHAN = ("Pyuthan", "086")
+    ROLPA = ("Rolpa", "086")
+    RUKUM_EAST = ("Rukum East", "088")
+    RUPANDEHI = ("Rupandehi", "071")
+
+    # Karnali Province
+    DAILEKH = ("Dailekh", "089")
+    DOLPA = ("Dolpa", "087")
+    HUMLA = ("Humla", "019")
+    JAJARKOT = ("Jajarkot", "089")
+    JUMLA = ("Jumla", "087")
+    KALIKOT = ("Kalikot", "087")
+    MUGU = ("Mugu", "019")
+    RUKUM_WEST = ("Rukum West", "088")
+    SALYAN = ("Salyan", "088")
+    SURKHET = ("Surkhet", "083")
+
+    # Sudurpashchim Province
+    ACHHAM = ("Achham", "097")
+    BAITADI = ("Baitadi", "095")
+    BAJHANG = ("Bajhang", "092")
+    BAJURA = ("Bajura", "097")
+    DADELDHURA = ("Dadeldhura", "096")
+    DARCHULA = ("Darchula", "093")
+    DOTI = ("Doti", "094")
+    KAILALI = ("Kailali", "091")
+    KANCHANPUR = ("Kanchanpur", "099")
+
+    def __init__(self, area: str, area_code: str):
+        self.area = area
+        self.area_code = area_code
+
+    def __str__(self):
+        return f"{self.area}: {self.area_code}"
+
+    @classmethod
+    def get_by_area_code(cls, area_code: str = None) -> list[str]:
+        if not area_code:
+            return []
+        results = [area.value[0] for area in cls if area.value[1] == area_code]
+        return results
+
+
 def is_mobile_number(number: str) -> bool:
     """
     Returns True is the input number is mobile number.
@@ -168,16 +275,31 @@ def _parse_landline_number(number) -> dict:
         "type": "Landline",
         "number": "98XXXXXXXX",
         "area_code": <AreaCode>
+        "area": list of areas/districts
     }
     """
-    number = get_exact_number(number)
 
     # adding zero
     if number[0] != "0":
         number = f"0{number}"
 
+    number = get_exact_number(number)
+    area_code = _get_area_code(number)
+    area = _get_area(area_code)
+
+    if not area:
+        return None
+
     return {
         "type": "Landline",
         "number": number,
         "area_code": _get_area_code(number),
+        "area": area,
     }
+
+
+def _get_area(area_code: str) -> list[str] | None:
+    """
+    Returns the details of the area code.
+    """
+    return Area.get_by_area_code(area_code)
